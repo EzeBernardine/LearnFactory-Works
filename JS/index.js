414fba4f49e5448b6d4e88c9e233dda7849e3a0f
EzeBernardine <demayous2@gmail.com>
2018-05-15 12:15:36 +0100

    added family and familyprofile fileand js file

---
+++ JS/index.js
@@ -0,0 +1,96 @@
+// alert("welcome to my world dear");
+// let name="925215";
+// console.log(typeof(name));
+// let user="emeke";
+// alert("good morning" + user);
+// let his="Mr Mathew";
+// //document.write("good"+ " " + "morning" + " " + " "  + his)
+
+// let nm="mathew onaja";
+// alert("hello"+nm);
+// alert(nm+""+"you are on the way to learnfactory");
+// let myPeople="Shalom bernardine Aoe and Grace";
+// document.write(typeof(myPeople)+" "+myPeople);
+
+// let num=24;
+// console.log(typeof(num));
+// let tostrin=String(num);
+// console.log(typeof(tostrin);
+// )
+
+// let string="sun" +" moon";
+// let size='how'
+// document.write(string + size +"<br>");
+// let d=3;
+// console.log(d--);
+
+let count=0;
+let username="blank";
+
+
+
+
+
+//using if statment
+// let corect="good";
+// let wrong="failed";
+// let  name=prompt("pls put your name to verify again");
+// if (name !== username ){
+//    alert(wrong);
+//    count++;
+//    if(count < 3){
+//     let  name=prompt("pls put your name to verify again");
+//     if(name !== username){
+//         alert(wrong);
+//         count++;
+//         if(count < 3){
+//             let  name=prompt("pls put your name to verify again");
+//             if(name !== username){
+//                 alert(wrong);
+//                 count++;
+//                 let  name=prompt("pls put your name to verify again");
+//                 if(count < 3){
+//                     if(name !== username){
+//                         alert(wrong);
+//                     }else{
+//                         alert(corect);
+//                         alert("you entered "+ count+"times");
+//                 }
+//             }else{
+//                 alert(corect);
+//             }
+//         }
+//     }else{
+//         alert(corect);
+//     }
+//    }
+// }else{
+//     alert("perfect");
+// }
+
+//let y="mat";
+//let z=(y==="mat") ? 22: 12;
+//alert(z)
+//let p=prompt("is permission grantede");
+//let age=18;
+//let c=prompt("what is the childs age");
+//if(c > age && p!==true){
+//    alert("permission denied");
+//    
+//}else if(c<age && p==true){
+//    alert("deniedd");
+//}
+//else if(c>=age && p!==true){
+//    alert("denied");
+//}
+//else if(c>=age && p==true){
+//    alert("granted");
+//}
+//else{
+//    alert("permission granted");
+//}
+   
+
+//let age=prompt("what is the childs age");
+//let permission=prompt("is he allowed");
+//let access=(age>=18) ? "you can go" : (permission="true")? alert("you have a parental //access"): "access denied;"
