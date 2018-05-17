4c71d124f00771ee64cb8710f994732af69539f8
EzeBernardine <demayous2@gmail.com>
2018-05-17 09:57:14 +0100

    assignmenton cases and initial toy numbers working mode

--- JS/index.js
+++ JS/index.js
@@ -148,45 +148,84 @@
 
 
 
-let number=prompt("input your account number");
-let active=prompt("is your account active"+ " "+ "\"yes or no\"") ;
-let result=(number=="2019"  &&  active=="yes")   ?   alert("your balance is 20"):  alert("in correct account detail");
+// let number=prompt("input your account number");
+// let active=prompt("is your account active"+ " "+ "\"yes or no\"") ;
+// let result=(number=="2019"  &&  active=="yes")   ?   alert("your balance is 20"):  alert("in correct account detail");
+
+
+
+// if(prompt("input your account number")=="2019"){
+//       alert("correct account number");
+//        if(prompt("input active account code") =="0012"){
+//             alert("your account balance is 20,000,000,000");
+//             alert("Sir Mathew, thank you for banking with us");
+//       }else{
+//             alert("you dont have an active account");
+//       }
+      
+// }else{
+//       alert("incorrect account number");
+      
+// }
+    
 
 
 
 
-let number=prompt("how many musicians are there");
-if(number==="0"){
-    alert("not a group");
-}
-    else if(number === "1"){
-        alert("solo");
-    }
-        else if(number == "2"){
-            alert("deux");
-        }
-        else if(number=="3"){
-            alert("trio");
-        }
-        else if(number=="4"){
-            alert("quartet");
-        }
-            else if(number > 4){
-               alert("the number is too large");
-            }
-                else {
-                   alert("what you entered"+ " " + number+ " "+ "is not a number");
-                }
 
-let number=prompt("pls enter a number");
-let result=number%2;
-if(result==0){
-      alert("thiis an even number");
-} else if (result!==0){
-      alert("thi is not an even number");
-} else{
-      alert("this is not a number");
-}
+
+
+// if(prompt("input your password number")=="2019"){
+//       alert("correct account number");
+//        if(prompt("input your drawer code") =="0012"){
+//             alert("you have won $20,000,000,000");
+//             alert("Miss Bernar, thank you for your effort");
+//       }else{
+//             alert("you dont have an active account");
+//       }
+      
+// }else{
+//       alert("incorrect account number");
+      
+// }
+
+
+
+
+
+// let number=prompt("how many musicians are there");
+// if(number==="0"){
+//     alert("not a group");
+// }
+//     else if(number === "1"){
+//         alert("solo");
+//     }
+//         else if(number == "2"){
+//             alert("deux");
+//         }
+//         else if(number=="3"){
+//             alert("trio");
+//         }
+//         else if(number=="4"){
+//             alert("quartet");
+//         }
+//             else if(number > 4){
+//                alert("the number is too large");
+//             }
+//                 else {
+//                    alert("what you entered"+ " " + number+ " "+ "is not a number");
+//                 }
+
+// let number=prompt("pls enter a number");
+// let result=number%2;
+// if(result==0){
+//       alert("thiis an even number");
+// } else if (result!==0 && !(isNaN(number))){
+//       alert("thi is not an even number");
+// } else if(isNaN(number)){
+//       alert("this is not a number");
+//     }
+
       
 
 
@@ -197,21 +236,86 @@ if(result==0){
 
 
 let num=prompt("enter a number");
-let find=(num.length);
-let toNumber=Number(num);
+let length=(num.length);
+ let toNumber=Number(num);
 
-if(find==2){
-     
-      alert(toNumber[0, 1]);
-}
+//let i=prompt("enter a number");
+let arr=["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+let fig=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let a=fig[0];
+let b=fig[1];
+let c=fig[2];
+let d=fig[3];
+let e=fig[4];
+let f=fig[5];
+let g=fig[6];
+let h=fig[7];
+let k=fig[8];
+let j=fig[9];
+
+
+let A=arr[0];
+let B=arr[1];
+let C=arr[2];
+let D=arr[3];
+let E=arr[4];
+let F=arr[5];
+let G=arr[6];
+let H=arr[7];
+let K=arr[8];
+let J=arr[9];
+
+ 
+
+
+ for(let i=0 ; i<=length ;  i++){
+      if(length==2  && num.charAt(0)==a   && num.charAt(1)==a){
+            alert( num.charAt(0) );
+        }else{
+             
+        }    
+                 
+      }
+
+
+let z="ty";
+let y="hundred";
+
+
+// alert(A + " " + B+ " " + C + " " + D+ " " + E+ " " + F+ " " + G+ " " + H+ " " + K+ " " + J);
 
 
 
+// do {alert
+//       yourName = prompt("Who are you?");
+//       } while (!yourName);
+//       console.log(yourName);
 
 
 
-
-
+ 
+// if(num[i]==a) {
+//       alert(A);
+// }else if(num[i]==b){
+//       alert(B);
+// }else if(num[i]==c){
+//       alert(C);
+// }else if(num[i]==d){
+//       alert(D);
+// }else if(num[i]==e){
+//       alert(E);
+// }else if(num[i]==f){
+//       alert(F);
+// }else if(num[i]==g){
+//       alert(G);
+// }else if(num[i]==h){
+//       alert(H);
+// }else if(num[i]==k){
+//       alert(K);
+// }else if(num[i]==j){
+//       alert(J);
+// } else{}
 
 
 //  if(num=="1"){
@@ -247,3 +351,106 @@ if(find==2){
 //                                  else{
 
 //                                  }
+
+
+// let months=prompt("what is the month of the year");
+// switch(months){
+//       case "july":
+// alert("the students are preparing for their third temexamination");
+// break;
+//       case "june":
+// alert("just rounded upthe first half of the year");
+// break;
+//       case "faburary":
+// alert("the fools are increasingin their numbers");
+// break;
+//       case "march":
+//       case "dec":
+// alert("I am marching forward");
+// break;
+//        case "may":
+// alert("its my birtday");
+// break;
+//       default:
+// alert("i dont know the season we are in");
+// break; 
+
+// }
+// let myArray="nop";
+
+// alert(myArray[0]);
+// alert(myArray[1]);
+
+// let u="one";
+// alert(u[0] && u[1]);
+
+
+
+// var browser = prompt("What is your browser type?");
+// switch (browser) {
+//   case "Edge":
+//     alert("You've got the Edge!");
+//     break;
+//   case "Chrome":
+//   case "Firefox":
+//   case "Safari":
+//   case "Opera":
+//     alert("Okay we support the browsers too");
+//     break;
+//   default:
+//     alert("We hope that this page looks ok!");
+// }
+// if (browser === "Edge") {
+//   alert("You've got the Edge!");
+// } else if (
+//   browser === "Chrome" ||
+//   browser === "Firefox" ||
+//   browser === "Safari" ||
+//   browser == "Opera"
+// ) {
+//   alert("Okay we support the browsers too");
+// } else {
+//   alert("We hope that this page looks ok!");
+// }
+
+
+
+// // QUESTION 2
+
+
+
+// let a = +prompt('a?', '');
+// if (a == 0) {
+//   alert(0);
+// }
+// if (a == 1) {
+//   alert(1);
+// }
+// if (a == 2 || a == 3) {
+//   alert('2, 3');
+// }
+// let a = +prompt("a?", "");
+
+// switch (a) {
+
+//   case 0:
+
+//     alert(0);
+
+//     break;
+
+//   case 1:
+
+//     alert(1);
+
+//     break;
+
+//   case 2:
+
+//   case 3:
+
+//     alert("2, 3");
+
+//     break;
+
+// }
