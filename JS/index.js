bb258fd1f3c78f28590b2f9b49fafa0e19497fd8
EzeBernardine <demayous2@gmail.com>
2018-05-23 17:28:10 +0100

    another toy question, questions on higher functions from Sir Matt and explanation of somehigher functions

--- JS/index.js
+++ JS/index.js
@@ -675,7 +675,6 @@ checkAge(age);
 
 
 //function expression
-
 let myCoursemate = function (x,y) {
       //expression
       let mssage = x + y;
@@ -686,12 +685,13 @@ myCoursemate(52, 536);
 
 //the forEach method
 const arr=["hello", "good", "morning"]
-arr.forEach(function(any, index, arr){
+arr.forEach(function(any, index, arr){//the major one isthe first: any. index and arr is optional. it just prints the indexes and the arrays
      console.log(any, index);
 });
 
 //filter using ES5
 let numbers=[52, 68, 5, 41, 2, 36, 2, 14, 64, 92, 62, 5, 632, 1, 4, 17, 1, 5, 84];
+alert(numbers[5]);
 let filt=numbers.filter(function(figure){
     if(figure>20){ 
     return true;}
@@ -703,14 +703,153 @@ let numbers=[8,96,4,96,456,85,4,12,8,7,85,96,4,6,8,62,630,620,26320];
 let filt=numbers.filter(figure=>figure>=85);
 console.log(filt);
 
-//the map function using ES5
+//the map function using ES6
 let resul=[{name:"adanme", age:"52", school:"lizzy"},
-          {name:"agaomuo", age: "12", school:"divine"},
-        {name:"ob, age",age:55, school:"oxford"}];
+           {name:"agaomuo", age: "12", school:"divine"},
+           {name:"ob, age",age:55, school:"oxford"}];
 let mapResult=resul.map(function(rer){
     return (rer.name+ rer.age+ rer.school);
 })
 console.log(mapResult);
+
+//sorting--the dash inbetwwen a & b means sort from lowest to highest' To sort from lowest tohighest, b-a
+let numbers=[8,96,4,96,456,85,4,12,8,7,85,96,4,6,8,62,630,620,26320];
+let sorrt=numbers.sort((a, b)=>(a-b));
+console.log(sorrt);
+
+//sorting sing arrow function
+let numbers=[845,41,4,96,456,85,4,12,8,7,85,96,4,6,8,62,630,620,26320];
+let sorrt=numbers.sort(function(a,b){//a here is 845 and b is 9. then a again is 845 6and b is 9
+    if(a<b){
+        return 1;
+        console.log(a +" "+b);
+    }else{ 
+        return -1;
+        console.log(a +" "+b);
+    }
+});
+console.log(sorrt);
+
+//TO ADD OR REMOVE FROM AN ARRAY
+//       slice
+//       splice
+
+//the splice array method---can delete and can add elements to an array
+arrayName.splice("the-index-number", "the-number-of-elementes-to-be-deleted", "the-element-you-want-to-add-seperated-with-commas")//the syntax. after deleting, the index is removed
+let numbers=[5,5,13,52,45,56];
+let sli=numbers.splice(0,3);//i am deleting from the zeroth index, three elements
+sli=numbers.splice(1,1);//from index 1, i removed one element
+sli=numbers.splice(2,0);//this will delete no element from index two
+sli=numbers.splice(2,0,"food");// from index two, delete zero number of element, and add the element food to that index
+sli=numbers.splice(-2,0,25);//two steps from the back to the frnt, delete none and insert 25//this will give you 5,5,13,52,25,45,56
+alert(numbers);
+let numbers=[5,5,13,52,45,56];
+delete numbers[3];//this method, deletes the array at the that index,and leaves the index empty. there is still the same number of elements after deleting
+alert(sli);
+alert(numbers);
+
+
+//slice---it returns a new array from the start index to the end(the end is not included)
+//the syntax
+arrayName.slice(start, end);//the start is the index where the slicing will commence and the end, the index before where it will stop
+let numbers=[85,6,16,4,6,45,4,68,84,12,4,5,854,85];
+let sli=numbers.slice(3,5);//this will give you 4,6
+alert(sli);
+//NOTE--the  new array returned converts all stings to just arrays--Eg
+let numbers=["20", "99", "46", "9", ];
+let sli=numbers.slice(1,3);// thi will give you 99,46
+sli=numbers.slice(-3);//when its a negative value, it starts counting from the back. It aslo makes usof one input ie -3
+alert(sli);
+
+
+//concat--it is used to join an array with other arrays or elements
+//syntax
+arrayName.concat(arguement1, arguement2...) 
+//NOTE --when you concat an array withanothe array, it give you a single array. And with some values, it also gives you a single array.
+let numbers=[5,5,13,52,45,56];
+let num=[5, 6, 263];
+let con=numbers.concat(num);//this will give you 5,5,13,52,45,56,5,6,263
+let conn=numbers.concat(num, 5,2,3);//this will output 5,5,13,52,45,56,5,6,263,5,2,3
+let combined={ 
+    name:"ada",
+    pasword:"hj852",
+}
+let combinedArrObj=numbers.concat(combined);//this will give you 5,5,13,52,45,56,[object Object]---this  is becausethe obj combined is an obhect, and does not have "symbol.isconcatspreadable"
+//NOTE--to concat an object array and have its element added, you input "Symbol.isConcatSpreadable"
+let combined={ 
+    0:"ada",
+    1:"hj852",
+    [Symbol.isConcatSpreadable] :true,
+    length:2
+};
+let numbers=[5,5,13,52,45,56];
+let combinedArrObj=numbers.concat(combined);// this output [5, 5, 13, 52, 45, 56, "ada", "hj852"]--this i because it contains the "[Symbol.isConcatSpreadable] :true," and "length:.."
+console.log(combinedArrObj);
+
+
+//To search from array
+//      indexOf--lastindexOf--include
+//      find---findIndez
+//      filter
+
+//indexOf--lastIndexOf---includes--all do similar things only on items and not character
+//it gets the index of the element
+//syntas
+arrayname.indexOf(item)
+// if the item is not found, -1is returned
+let numbers=[5,6,1,1,0,61,42,4];
+let ind=numbers.indexOf(1);//this will output 2
+//includescheks if the element in question ispresent  in the array,not the index
+let present=numbers.includes(4);//this will output true
+alert(present);
+
+//find--findIndex---It is used in an array of objects to find an object
+//syntas
+.//find returns one element
+arrayName.find(function(anyWord, index, array){anyWord=="pick any word in the array"})
+let customer=[
+    {id: 001, name: "Ada Mike", product: "Milk", email: "ada@gmail.com" },
+    {id: 002, name: "Friday Chukwuma", product: "Milk", email: "friday@gmail.com" },
+    {id: 003, name: "Ifeanyi Okeke", product: "Indomie", email: "ifeanyi@gmail.com" },
+    {id: 004, name: "Michael Chilaka", product: "Milk", email: "michael@gmail.com" },
+    {id: 001, name: "Bitrus Abah", product: "Fruits", email: "bitrus@gmail.com" },
+]
+let fin=customer.find(item=>item.id=="003");//this will output ifeanyi okeke 
+ //findIndex returns the index where the element wrer foun
+alert(fin.name+index+array);
+
+//filter ----it returns an array, and element and/or the index
+//syntax
+arrayName.filter(any, index, array=>any.sth..)
+let customer=[
+    {id: 001, name: "Ada Mike", product: "Milk", email: "ada@gmail.com" },
+    {id: 002, name: "Friday Chukwuma", product: "Milk", email: "friday@gmail.com" },
+    {id: 003, name: "Ifeanyi Okeke", product: "Indomie", email: "ifeanyi@gmail.com" },
+    {id: 004, name: "Michael Chilaka", product: "Milk", email: "michael@gmail.com" },
+    {id: 005, name: "Bitrus Abah", product: "Fruits", email: "bitrus@gmail.com" },
+    {id: 006, name: "Ada Ebuka", product: "Rice", email: "ada@gmail.com" },
+    {id: 007, name: "Olodo Hen", product: "Beans", email: "olodo@gmail.com" },
+    {id: 007, name: "Love You", product: "Garri", email: "love@gmail.com" },
+    {id: 007, name: "Cherish You", product: "Orange", email: "cherish@gmail.com" },
+    {id: 010, name: "Beautiful You", product: "Sugar", email: "beautiful@gmail.com" },
+]
+let filt=customer.filter(index=>index.id < 010);
+alert(filt.length)//this will output 9
+let numbers=[5,5,13,52,45,56];
+let fil=numbers.filter(index=>index>10)//this will outpt 13,52,45,56
+alert(fil);
+
+
+//totransform an array or reorder it
+//      map
+//      sort
+
+//the map
+//the syntax
+let result = arr.map(function(item, index, array)
+alert()
+
+
 
 //objexct
 let array=[];//for array
@@ -726,4 +865,127 @@ alert(objString.name + objString.email) //obidemayous
 alert("name"+" "+objString.name+" " +"email" +" " +objString.email)              
 
 //using literal strings rather than ""
-alert(`Name:${objString.name} Email:${objString.email}`)
+alert(`Name:${objString.name} Email:${objString.email}`)//Name:obi Email:demayous
+
+
+//bracket notation
+alert(objString["name"]);//obi
+
+//using functions with objects--------
+var data={
+    name:ada,
+    age:56
+    };
+addNumbers
+
+ //adding properties to an empty dbject
+let obj={};
+obj.username ="obi";//i have assigned a key-username and a value obi
+obj.password="hjk741";
+
+//looping through an object
+let objString={name:"obi",
+               email:"demayous",
+               password:"hj852",
+               adress:"udogen street"};
+for(let prop in objString){//this is the {for in }method
+    console.log(prop)// i am looping throughthe object and i am alerting all the propertis
+    console.log(objString[prop]);// thiswill give me their keyes
+}
+
+//changing the keyes of an object and assigning them to a new object
+let newobj=objString;
+newobj.name="emeka";
+newobj.password="ghj5";
+alert(objString.name);
+
+//function
+function names(name1, name2){
+    return{
+        fname:name1,
+        lname:name2
+    }
+}
+let myname=names("jude", "prince");
+console.log(myname.fname);
+
+//using Es6
+function names(name1, name2){
+    return{
+        name1,
+        name2
+    }
+}
+let myname=names("jude", "prince");
+console.log(myname());
+
+//cloning an object
+let emptyobj={};
+for(let key in names){
+    emptyobj[key]=names[key];
+}
+
+
+//using a shorter way
+let emptyobj={};
+Object.assign(destination[objec1,objec2]);
+Object.assign(emptyobj, names);// you are assigning  the value of
+
+//caviates-----obj are stored by reference while primitive data type is stored by the actual values
+function print(){
+    var name="victor";
+    var file;
+}
+print();
+console.log(name);
+console.log(file);
+
+//mix of values
+let arr = [ 'Apple', { name: 'John' }, true, function() { alert('hello'); } ];
+// get the object at index 1 and then show its name
+alert( arr[1].name ); // John
+// get the function at index 3 and run it
+arr[3](); // hello
+
+//classwork
+var mtArray=[
+    [23, 156, 25,10, 52, 23],
+    [12, 100, 23, 56, 81, 93],
+    [42.5, 71, 10, 23, 35, 11, 72, 99],
+    [11, 100, 99, 102, 13, 8, 12]
+];
+//1. Given the array of numbers above, write a javascript function that accepts the array,create a new array whose element are 20% of the original array.
+
+//2. The "myArray" variable is multidimensional array.Use a nested `for` loop to cycle through `myArray`.Convert each even number to the string "even"Convert each odd number to the string "odd",
+var myArray = [
+    [23, 156, 25, 10, 52, 23],
+    [12, 100, 23, 56, 81, 93],
+    [42.5, 71, 10, 23, 35, 11, 72, 99],
+    [11, 100, 99, 102, 13, 8, 12]
+  ]
+
+// No 3
+//Assuming the database below is your database:
+//(a)Find  a particular customer in the database whose iD is 003
+//(b)Find all customers whose ID is less than 010
+//(c)create a new database containing customers whose name contains the leter A
+let customer=[
+    {id: 001, name: "Ada Mike", product: "Milk", email: "ada@gmail.com" },
+    {id: 002, name: "Friday Chukwuma", product: "Milk", email: "friday@gmail.com" },
+    {id: 003, name: "Ifeanyi Okeke", product: "Indomie", email: "ifeanyi@gmail.com" },
+    {id: 004, name: "Michael Chilaka", product: "Milk", email: "michael@gmail.com" },
+    {id: 005, name: "Bitrus Abah", product: "Fruits", email: "bitrus@gmail.com" },
+    {id: 006, name: "Ada Ebuka", product: "Rice", email: "ada@gmail.com" },
+    {id: 007, name: "Olodo Hen", product: "Beans", email: "olodo@gmail.com" },
+    {id: 007, name: "Love You", product: "Garri", email: "love@gmail.com" },
+    {id: 007, name: "Cherish You", product: "Orange", email: "cherish@gmail.com" },
+    {id: 010, name: "Beautiful You", product: "Sugar", email: "beautiful@gmail.com" },
+]
+
+//solution
+//3a
+let fin=customer.find(index=>index.id==003);//this will output 4
+alert(fin.name);
+
+ //TOY question number two
+ //determine if a string is a palidrum or not with using higher functions
