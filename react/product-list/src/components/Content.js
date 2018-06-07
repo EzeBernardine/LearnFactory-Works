6afb53928e0ec877fae35da06512562d55a4a15e
EzeBernardine <demayous2@gmail.com>
2018-06-07 12:05:22 +0100

    react classon component

---
+++ react/product-list/src/components/Content.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+
+export default class Content extends Component{
+    componentWillMount(){
+        console.log("component will mount")
+    }
+    componentDidMount(){
+        console.log("componeont did mount")
+    }
+    componentWIllReceiveProps(newProps){
+        console.log("component will recieve props")
+    }
+    shouldComponentUpdate(newProps, newState){
+        return true;
+    }
+    componentWillUpdate(nextProps, nextState){
+        console.log("components will update")
+    }
+    componentDIdUpdate(prevsProps, prevState){
+        console.log("components did update")
+    }
+    render(){
+        return(
+            <div>
+                <h3>{this.props.myNumber}</h3>
+            </div>
+        )
+    }
+}
